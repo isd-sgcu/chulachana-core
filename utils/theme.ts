3232c366f2952ddda370cce81bd4b98cf22c3c0d
paphonb <paphonb@gmail.com>
2020-10-24 16:01:59 +0700

    Use Roboto for non-Thai characters and add fallback fonts

--- utils/theme.ts
+++ utils/theme.ts
@@ -2,6 +2,9 @@ import { ThemeOptions } from '@material-ui/core'
 
 export const appThemeOptions: ThemeOptions = {
   typography: {
-    fontFamily: 'Noto Sans Thai',
+    fontFamily:
+      'Noto Sans Thai, Sukhumvit Set, -apple-system, BlinkMacSystemFont, ' +
+      'Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, ' +
+      'Helvetica Neue, sans-serif',
   },
 }
