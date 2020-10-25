e490a9c621505bbc9997af9e45dc70e71303d08a
paphonb <paphonb@gmail.com>
2020-10-25 13:25:05 +0700

    Add production build config

---
+++ next.config.js
@@ -0,0 +1,6 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = (phase, defaultConfig) => {
+  return withBundleAnalyzer(defaultConfig)
+}
