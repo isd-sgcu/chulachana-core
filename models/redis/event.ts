ac47e72c6ab5b6d74594439b917daef54eec3107
Nut Pinyo <bomb.np@gmail.com>
2022-03-14 17:05:34 +0700

    feat: use model in index.tsx

--- models/redis/event.ts
+++ models/redis/event.ts
@@ -1,11 +1,26 @@
 import { redisClient } from '../clients'
 
 export async function getEvents() {
-  const events = await redisClient.LRANGE('config:core:eventList', 0, -1)
+  const eventIds = await redisClient.LRANGE('config:core:eventList', 0, -1)
+  const events = await Promise.all(
+    eventIds.map(async (eventId) => getEventInfo(eventId))
+  )
   return events
 }
 
-export async function getEventConfig(eventId: string) {
+export interface EventInfo {
+  id: string
+  name: string
+  primaryColor: string
+  secondaryColor: string
+}
+
+export async function getEventInfo(eventId: string): Promise<EventInfo> {
   const config = await redisClient.HGETALL(`config:${eventId}`)
-  return config
+  return {
+    id: eventId,
+    name: config.name,
+    primaryColor: config.primaryColor,
+    secondaryColor: config.secondaryColor,
+  }
 }
