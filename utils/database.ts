127ff7f3989d41b7d64300c4fb2021bf55773f06
Nut Pinyo <bomb.np@gmail.com>
2022-03-14 13:53:30 +0700

    feat: use singleton pattern orn redis client

--- utils/database.ts
+++ utils/database.ts
@@ -2,16 +2,20 @@ import { InfluxDB } from '@influxdata/influxdb-client'
 import * as redis from 'redis'
 import { config } from './env'
 
-export const createRedisClient = async () => {
-  const client = redis.createClient({
-    socket: {
-      host: config.redis.host,
-      port: config.redis.port,
-    },
-    database: config.redis.database,
-  })
-  await client.connect()
-  return client
+let redisClient: redis.RedisClientType
+
+export const getRedisClient = async () => {
+  if (redisClient) {
+    redisClient = redis.createClient({
+      socket: {
+        host: config.redis.host,
+        port: config.redis.port,
+      },
+      database: config.redis.database,
+    })
+    await redisClient.connect()
+  }
+  return redisClient
 }
 
 export const influxClient = new InfluxDB({
