956c9d8d22069ebbe58b8e024d45ae893926f09b
Nut Pinyo <bomb.np@gmail.com>
2022-03-16 16:09:31 +0700

    feat: update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,100 @@
-import { PrismaClient } from '@prisma/client'
+import {
+  Entry,
+  EntryType,
+  Event,
+  Prisma,
+  PrismaClient,
+  Role,
+  User,
+  UserInEvent,
+  Year,
+} from '@prisma/client'
 
 const prisma = new PrismaClient()
+
+interface CreateRoleProps {
+  event: Event
+  slug: string
+  name: string
+}
+
+async function createRole({ event, slug, name }: CreateRoleProps) {
+  let a: Prisma.EventWhereUniqueInput
+  const role = await prisma.role.create({
+    data: {
+      slug,
+      name,
+      event: { connect: { id: event.id } },
+    },
+  })
+  return role
+}
+
+interface CreateUserProps {
+  phone: string
+  name: string
+  faculty?: string
+  year?: Year
+}
+
+async function upsertUser({ phone, name, faculty, year }: CreateUserProps) {
+  const user = await prisma.user.upsert({
+    where: { phone },
+    create: {
+      phone,
+      name,
+      faculty,
+      year,
+    },
+    update: {},
+  })
+  return user
+}
+
+interface CheckProps {
+  user: User
+  event: Event
+  role: Role
+  type: EntryType
+}
+
+async function check({
+  user,
+  event,
+  role,
+  type,
+}: CheckProps): Promise<[Entry, UserInEvent]> {
+  const [entry, userInEvent] = await prisma.$transaction([
+    prisma.entry.create({
+      data: {
+        user: { connect: { id: user.id } },
+        event: { connect: { id: event.id } },
+        role: { connect: { id: role.id } },
+        timestamp: Math.round(Date.now() / 1000),
+        type,
+      },
+    }),
+    prisma.userInEvent.upsert({
+      where: {
+        userId_eventId: {
+          userId: user.id,
+          eventId: event.id,
+        },
+      },
+      create: {
+        user: { connect: { id: user.id } },
+        event: { connect: { id: event.id } },
+        role: { connect: { id: role.id } },
+      },
+      update: {
+        user: { connect: { id: user.id } },
+        event: { connect: { id: event.id } },
+        role: { connect: { id: role.id } },
+      },
+    }),
+  ])
+  return [entry, userInEvent]
+}
 
 async function seed() {
   const event = await prisma.event.upsert({
@@ -11,13 +105,62 @@ async function seed() {
       name: 'Mind Market',
       primaryColor: '3744AC',
       secondaryColor: 'F2929D',
-      roles: {
-        create: [
-          { slug: 'visitor', name: 'ผู้เข้าร่วมงาน' },
-          { slug: 'staff', name: 'ผู้ปฏิบัติงาน' },
-        ],
-      },
     },
+  })
+
+  await prisma.role.deleteMany({})
+  const roleVisitor = await createRole({
+    event,
+    slug: 'visitor',
+    name: 'ผู้เข้าร่วมงาน',
+  })
+  const roleCUStaff = await createRole({
+    event,
+    slug: 'custaff',
+    name: 'นิสิตผู้ปฏิบัติงาน',
+  })
+  const roleExtStaff = await createRole({
+    event,
+    slug: 'extstaff',
+    name: 'เจ้าหน้าที่ภายนอก',
+  })
+
+  const user1 = await upsertUser({
+    phone: '0812345678',
+    name: 'John Doe',
+  })
+  const user2 = await upsertUser({
+    phone: '0823456789',
+    name: 'Jane Doe',
+  })
+  const user3 = await upsertUser({
+    phone: '0834567890',
+    name: 'Jack Doe',
+  })
+
+  await check({
+    user: user1,
+    event,
+    role: roleVisitor,
+    type: EntryType.IN,
+  })
+  await check({
+    user: user2,
+    event,
+    role: roleCUStaff,
+    type: EntryType.IN,
+  })
+  await check({
+    user: user1,
+    event,
+    role: roleVisitor,
+    type: EntryType.OUT,
+  })
+  await check({
+    user: user1,
+    event,
+    role: roleVisitor,
+    type: EntryType.IN,
   })
 }
 
