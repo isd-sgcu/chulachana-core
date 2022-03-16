9572bcdcb1b1a9ea630a02479bcc6e812bf71fe8
samithiwat <samithiwat2544@gmail.com>
2022-03-16 13:37:10 +0700

    Refactor check function

--- api/check.ts
+++ api/check.ts
@@ -1,47 +1,45 @@
-import { HttpError, Point } from '@influxdata/influxdb-client'
-import { influxClient } from '../models/clients'
-import { config } from '../utils/env'
-import { ApiError, PointUserDto } from '../utils/types'
+import { Entry, EntryType, Prisma, Role, User } from '@prisma/client'
+import { createEntry, dtoToRawEntry } from '../models/prisma/entry'
+import { EventInfo, getEventInfo } from '../models/prisma/event'
+import { createUser, findUserByPhone } from '../models/prisma/user'
+import { CheckinDTO } from '../pages/api/checkin'
+import { ApiError, UserInfo } from '../utils/types'
 
 export async function check(
-  eventid: string,
-  phone: string,
-  type: string,
-  isCheckin: 0 | 1
+  checkinDto: CheckinDTO,
+  type: EntryType
 ): Promise<Date> {
   // Initialize
-  const writeApi = influxClient.getWriteApi(
-    config.influx.organization,
-    config.influx.bucketPrefix + eventid,
-    'ns'
-  )
-
-  // Data Assigning
-  const pointDto = <PointUserDto>{
-    _time: new Date(),
-    _measurement: 'user',
-    eventid: eventid,
-    phone: phone,
-    type: type,
-    _field: 'in_event',
-    _value: isCheckin,
+  const userInfo: UserInfo = {
+    faculty: checkinDto.faculty,
+    name: checkinDto.name,
+    phone: checkinDto.phone,
+    year: checkinDto.year,
   }
 
-  const point = new Point(pointDto._measurement)
-    .tag('phone', pointDto.phone)
-    .tag('type', pointDto.type)
-    .intField(pointDto._field, pointDto._value)
-    .timestamp(pointDto._time)
+  const event: EventInfo = await getEventInfo(checkinDto.eventId)
+  if (!event) {
+    throw new ApiError(404, 'Event Not Found')
+  }
+
+  const role = event.roles.find((role) => role.slug === checkinDto.role)
 
   // Database Writing
-  writeApi.writePoint(point)
+  let user: User
   try {
-    await writeApi.close()
-  } catch (e) {
-    if (e instanceof HttpError && e.statusCode === 404) {
-      throw new ApiError(404, 'EventID Not Found')
-    }
-    throw new ApiError(500, 'Internal Error')
+    user = await createUser(userInfo)
+  } catch (err) {
+    user = await findUserByPhone(checkinDto.phone)
   }
-  return pointDto._time
+
+  const entryInfo: Prisma.EntryCreateInput = await dtoToRawEntry(
+    checkinDto,
+    user,
+    role as Role,
+    type
+  )
+
+  const entry: Entry = await createEntry(entryInfo)
+
+  return new Date(entry.timestamp * 1000)
 }
